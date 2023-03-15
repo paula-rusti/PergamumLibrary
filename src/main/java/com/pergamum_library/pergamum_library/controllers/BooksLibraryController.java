6d0a0225a824f49cac23643ac8513500c87399af
Paula Rusti <rusti.paula78@gmail.com>
2023-03-16 00:12:59 +0200

    made book class an entity

--- src/main/java/com/pergamum_library/pergamum_library/controllers/BooksLibraryController.java
+++ src/main/java/com/pergamum_library/pergamum_library/controllers/BooksLibraryController.java
@@ -7,17 +7,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 @RestController
 public class BooksLibraryController {
 
     static ArrayList<Book> database = new ArrayList<>();
-    static {
-        database.add(new Book("t1", "a1"));
-        database.add(new Book("t2", "a2"));
-    }
 
     @GetMapping("/books/")
     public List<Book> getBooks() {
--- src/main/java/com/pergamum_library/pergamum_library/entities/Book.java
+++ src/main/java/com/pergamum_library/pergamum_library/entities/Book.java
@@ -1,16 +1,17 @@
 package com.pergamum_library.pergamum_library.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
     private String title;
     private String author;
-
-    public Book() {
-
-    }
-    public Book(String title, String author) {
-        this.title = title;
-        this.author = author;
-    }
 
     public String getTitle() {
         return title;
@@ -26,5 +27,9 @@ public class Book {
 
     public void setAuthor(String author) {
         this.author = author;
+    }
+
+    public long getId() {
+        return id;
     }
 }

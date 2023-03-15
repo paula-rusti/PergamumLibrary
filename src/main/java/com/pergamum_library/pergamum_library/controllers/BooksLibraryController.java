38d7d1e7f7aaecb478e6f3a5999fdebc67c9c178
Paula Rusti <rusti.paula78@gmail.com>
2023-03-15 23:52:55 +0200

    added dummy add operation

--- src/main/java/com/pergamum_library/pergamum_library/controllers/BooksLibraryController.java
+++ src/main/java/com/pergamum_library/pergamum_library/controllers/BooksLibraryController.java
@@ -2,6 +2,8 @@ package com.pergamum_library.pergamum_library.controllers;
 
 import com.pergamum_library.pergamum_library.entities.Book;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -22,5 +24,9 @@ public class BooksLibraryController {
         return database;
     }
 
-
+    @PostMapping("/books/")
+    public Book addBook(@RequestBody Book book) {
+        database.add(book);
+        return book;
+    }
 }

868e56e7a32149593dbeb3dc0dc32b205badb525
Paula Rusti <rusti.paula78@gmail.com>
2023-03-15 23:47:35 +0200

    added dummy in-memory db

--- src/main/java/com/pergamum_library/pergamum_library/controllers/BooksLibraryController.java
+++ src/main/java/com/pergamum_library/pergamum_library/controllers/BooksLibraryController.java
@@ -1,12 +1,26 @@
 package com.pergamum_library.pergamum_library.controllers;
 
+import com.pergamum_library.pergamum_library.entities.Book;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 @RestController
 public class BooksLibraryController {
-    @GetMapping("/books/")
-    public String getBooks() {
-        return "I will return all the books";
+
+    static ArrayList<Book> database = new ArrayList<>();
+    static {
+        database.add(new Book("t1", "a1"));
+        database.add(new Book("t2", "a2"));
     }
+
+    @GetMapping("/books/")
+    public List<Book> getBooks() {
+        return database;
+    }
+
+
 }

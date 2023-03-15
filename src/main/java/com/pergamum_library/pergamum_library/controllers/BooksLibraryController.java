cdf33e8189d290174debf5d6322a691363c50418
Paula Rusti <rusti.paula78@gmail.com>
2023-03-16 00:34:11 +0200

    added functionality to find book by title

--- src/main/java/com/pergamum_library/pergamum_library/controllers/BooksLibraryController.java
+++ src/main/java/com/pergamum_library/pergamum_library/controllers/BooksLibraryController.java
@@ -3,10 +3,8 @@ package com.pergamum_library.pergamum_library.controllers;
 import com.pergamum_library.pergamum_library.entities.Book;
 import com.pergamum_library.pergamum_library.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,16 +14,22 @@ public class BooksLibraryController {
     @Autowired
     BookRepository bookRepository;
 
-    @GetMapping("/books/")
-    public List<Book> getBooks() {
-        return (List<Book>) bookRepository.findAll();
-    }
-
     @PostMapping("/books/")
     public Book addBook(@RequestBody Book book) {
         bookRepository.save(book);
         return book;
     }
+
+    @GetMapping("/books/")
+    public List<Book> getBooks() {
+        return (List<Book>) bookRepository.findAll(Sort.by("author", "title"));
+    }
+
+    @GetMapping("/books/search")
+    public List<Book> searchBooks(@RequestParam String title) {
+        return (List<Book>) this.bookRepository.findBookByTitle(title);
+    }
+
 
 
 }
--- src/main/java/com/pergamum_library/pergamum_library/repository/BookRepository.java
+++ src/main/java/com/pergamum_library/pergamum_library/repository/BookRepository.java
@@ -1,11 +1,14 @@
 package com.pergamum_library.pergamum_library.repository;
 
 import com.pergamum_library.pergamum_library.entities.Book;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 // will be implemented, instantiated and injected by spring in the controller
 @Repository
 public interface BookRepository extends CrudRepository<Book, Long> {
-    Iterable<Book> findAll();
+    Iterable<Book> findAll(Sort sort); // used for getting all the books sorted by author and title
+
+    Book findBookByTitle(String title); // used to retrieve a book from the library based on its name
 }

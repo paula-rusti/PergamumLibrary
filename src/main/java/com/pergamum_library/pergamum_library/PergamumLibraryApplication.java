278cdc6e14cc693a9b34735a0381d3db4573f0a0
Paula Rusti <rusti.paula78@gmail.com>
2023-03-15 22:54:04 +0200

    initial commit

---
+++ src/main/java/com/pergamum_library/pergamum_library/PergamumLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.pergamum_library.pergamum_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PergamumLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PergamumLibraryApplication.class, args);
+	}
+
+}
